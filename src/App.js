9b5543cd0213bd1b4683e8f7acc0b0572f27a758
Alexey Zakharchuk <alexey.zak@architech.nyc>
2021-05-02 19:34:02 +0300

    add select

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // MAKE BUTTONS AND INITIAL NAV PRETTIER https://material-ui.com/ru/components/app-bar/
 
-import "./App.css"
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -20,32 +19,13 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
-import Modal from '@material-ui/core/Modal'
-
-let initialModalState = {title: "", description: "", button: undefined}
-
+import {Popup} from "./components/Popup"
+import {products} from "./static/products"
+import {Header} from "./components/Header"
+import {initialModalState} from "./components/Popup"
 
 export default function App() {
-  const products = [{
-    id: 0,
-    name: "Vacuum",
-    description: "lorem ipsum dolorem est lorem ipsum dolorem est lorem ipsum dolorem est lorem ipsum dolorem est lorem ipsum dolorem est lorem ipsum dolorem est",
-    price: 1000,
-    img: "https://www.shespeaks.com/pages/img/review/Eureka%20Pet%20Pal:Paw%20Vaccuum_09082009162421.jpg"
-  }, {
-    id: 104,
-    name: "Swiffer",
-    description: "lorem ipsum dolorem est lorem ipsum dolorem est lorem ipsum dolorem est lorem ipsum dolorem est lorem ipsum dolorem est lorem ipsum dolorem est",
-    price: 40500,
-    img: "https://b3h2.scene7.com/is/image/BedBathandBeyond/106058914419080p?$690$&wid=690&hei=690"
-  }, {
-    id: 18,
-    name: "Mop",
-    description: "lorem ipsum dolorem est lorem ipsum dolorem est lorem ipsum dolorem est lorem ipsum dolorem est lorem ipsum dolorem est lorem ipsum dolorem est",
-    price: 300,
-    img: "https://cdn.shopify.com/s/files/1/0280/4006/9155/products/Microfiber-Mop-System.jpg?v=1609258501"
-  }]
-  let [modalState, setModalState] = React.useState(initialModalState)
+  const [modalState, setModalState] = React.useState(initialModalState)
   const [cart, setCart] = React.useState([])
   const addToCart = (productId) => {
     const selectedProduct = products.find((value) => {
@@ -75,38 +55,16 @@ export default function App() {
       cart.map((value) => (value.id == productId ? {...value, quantity} : value)))
   }
 
-const updateModalState = (state) => {
-  setModalState(state || initialModalState)
-}
+  const updateModalState = (state) => {
+    setModalState(state || initialModalState)
+  }
  
   return (
     <Router>
-      <Modal
-          open={Boolean(modalState.title)}
-          onClose={() => updateModalState()}>
-            <div className="modalPopup">  
-              <h3>{modalState.title}</h3>
-              <div>{modalState.description}</div>
-              {modalState.button}
-            </div>
-      </Modal>
+      <Popup {...modalState} onClose={() => updateModalState()} />
       <div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink exact={true} to="/" activeClassName="active">Home</NavLink>
-            </li>
-            <li>
-              <NavLink exact={true} to="/cart" activeClassName="active">Cart</NavLink>
-            </li>
-            <li>
-              <NavLink exact={true} to="/products" activeClassName="active">Products</NavLink>
-            </li>
-          </ul>
-        </nav>
+        <Header />
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <AppContext.Provider value={{products, addToCart, deleteFromCart, cart, changeQuantity, updateModalState}}>
           <main>
             <Switch>
--- src/components/pages/product/ProductPage.js
+++ src/components/pages/product/ProductPage.js
@@ -7,19 +7,24 @@ import {
 } from "react-router-dom"
 import IconButton from '@material-ui/core/IconButton'
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import {Select} from "../../UI/Select"
 
 export const ProductPage = () => {
     const {productId} = useParams()
-    const {products} = React.useContext(AppContext)
-    const {addToCart} = React.useContext(AppContext)
+    const {products, addToCart} = React.useContext(AppContext)
+
     const selectedProduct = products.find((value) => {
         return value.id == productId
     })
+
+    const characters = Object.keys(selectedProduct.characters || {})
+
     return (
         <div>
             <h1>{selectedProduct.name}</h1>
             <img src={selectedProduct.img}/>
             <p>{selectedProduct.description}</p>
+            <Select selected={'red'} title="Color" list={['red', 'green']} onSelect={() => {}} />
             <IconButton color="primary" aria-label="add to shopping cart" onClick={() => addToCart(selectedProduct.id)}>
                 <AddShoppingCartIcon />
             </IconButton>
